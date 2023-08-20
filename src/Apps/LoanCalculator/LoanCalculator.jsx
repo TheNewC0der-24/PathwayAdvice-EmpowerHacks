0d0ee514ebf0fcbd1aa87eac314e293c0ae52134
idsus <susmitkdas@gmail.com>
2023-08-20 14:12:38 -0700

    Edit

--- src/Apps/LoanCalculator/LoanCalculator.jsx
+++ src/Apps/LoanCalculator/LoanCalculator.jsx
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import {
-    Box, Typography,
+    Box, Typography, TextField, Button,
 } from '@mui/material';
 
 const LoanCalculator = () => {
+    const [loanAmount, setLoanAmount] = useState('');
+    const [interestRate, setInterestRate] = useState('');
+    const [loanTerm, setLoanTerm] = useState('');
+    const [monthlyPayment, setMonthlyPayment] = useState(null);
+
+    const calculateMonthlyPayment = () => {
+        const r = interestRate / 100 / 12;
+        const n = loanTerm * 12;
+        const payment = (loanAmount * r) / (1 - Math.pow((1 + r), -n));
+        setMonthlyPayment(payment.toFixed(2));
+    };
+
     return (
         <Box mt={5} p={2} sx={{
             bgcolor: "#F3F0FF",
@@ -13,19 +25,57 @@ const LoanCalculator = () => {
             placeItems: "center",
         }}>
             <Typography sx={{ fontWeight: "bold" }} variant="subtitle1">
-                STAY TUNED
+                Loan Calculator
             </Typography>
 
-            <Typography mt={2} variant="h5">
-                Loan Calculator Coming Soon!
-            </Typography>
+            <TextField
+                label="Loan Amount"
+                variant="outlined"
+                type="number"
+                value={loanAmount}
+                onChange={(e) => setLoanAmount(e.target.value)}
+                sx={{ mt: 2, width: '80%' }}
+            />
+
+            <TextField
+                label="Interest Rate (%)"
+                variant="outlined"
+                type="number"
+                value={interestRate}
+                onChange={(e) => setInterestRate(e.target.value)}
+                sx={{ mt: 2, width: '80%' }}
+            />
+
+            <TextField
+                label="Loan Term (years)"
+                variant="outlined"
+                type="number"
+                value={loanTerm}
+                onChange={(e) => setLoanTerm(e.target.value)}
+                sx={{ mt: 2, width: '80%' }}
+            />
+
+            <Button
+                variant="contained"
+                color="primary"
+                onClick={calculateMonthlyPayment}
+                sx={{ mt: 2 }}
+            >
+                Calculate
+            </Button>
+
+            {monthlyPayment && (
+                <Typography mt={2} variant="h5">
+                    Monthly Payment: ${monthlyPayment}
+                </Typography>
+            )}
 
             <Typography mt={2} align='center' variant="body1">
-                We are a small team of developers working on this project in our free time. We are working hard to bring you this feature as soon as possible.
+                Please ensure the values entered are correct.
             </Typography>
 
             <Typography mt={2} sx={{ fontWeight: "bold" }} color="#6d2ae2" variant="caption">
-                #underconstruction
+                Note: This is a basic calculator and might not include all aspects of your loan.
             </Typography>
         </Box>
     )
--- src/Components/Tools/Tools.jsx
+++ src/Components/Tools/Tools.jsx
@@ -1,13 +1,119 @@
-import React from 'react'
+import React, { useState } from 'react';
+
+import {
+    Box,
+    Typography,
+    Avatar,
+    Divider,
+    Button
+} from '@mui/material';
+
+import { RiToolsFill } from 'react-icons/ri';
+import { MdLock } from 'react-icons/md';
+
+import BudgetApp from '../../Apps/BudgetApp/BudgetApp';
+import LoanCalculator from '../../Apps/LoanCalculator/LoanCalculator';
 
 const Tools = () => {
 
     const [active, setActive] = useState("budget");
 
     return (
-        <div>
-            Tools
-        </div>
+        <React.Fragment>
+            <Box sx={{ display: "flex", alignItems: "center", justifyContent: "space-between", flexWrap: "wrap" }}>
+                <Box sx={{ display: "flex", alignItems: "center", gap: '0.5rem' }}>
+                    <Avatar variant="rounded" sx={{ color: "#6d2ae2", bgcolor: "#dfd8fd" }}>
+                        <RiToolsFill />
+                    </Avatar>
+                    <Typography variant='h5' sx={{ fontWeight: "bold" }} color="#6d2ae2">
+                        Tools
+                    </Typography>
+                </Box>
+            </Box>
+            <Divider sx={{ mt: 3, mb: 5 }} />
+
+            <Box>
+                <Button
+                    variant={active === "budget" ? "contained" : "outlined"}
+                    sx={{
+                        mr: 2, mb: 2,
+                        backgroundColor: active === "budget" ? "#6d2ae2" : "transparent",
+                        color: active === "budget" ? "#fff" : "#6d2ae2",
+                        borderColor: "#6d2ae2",
+                        '&:hover': {
+                            backgroundColor: active === "budget" ? "#6d2ae2" : "#dfd8fd",
+                            color: active === "budget" ? "#fff" : "#6d2ae2",
+                            borderColor: "#6d2ae2",
+                        }
+                    }}
+                    onClick={() => setActive("budget")}
+                >
+                    Budget App
+                </Button>
+
+                <Button
+                    variant={active === "loan" ? "contained" : "outlined"}
+                    sx={{
+                        mr: 2, mb: 2,
+                        backgroundColor: active === "loan" ? "#6d2ae2" : "transparent",
+                        color: active === "loan" ? "#fff" : "#6d2ae2",
+                        borderColor: "#6d2ae2",
+                        '&:hover': {
+                            backgroundColor: active === "loan" ? "#6d2ae2" : "#dfd8fd",
+                            color: active === "loan" ? "#fff" : "#6d2ae2",
+                            borderColor: "#6d2ae2",
+                        }
+                    }}
+                    color="primary"
+                    onClick={() => setActive("loan")}
+                >
+                    Loan Calculator
+                </Button>
+
+                <Button
+                    variant={active === "fitness" ? "contained" : "outlined"}
+                    sx={{
+                        mr: 2, mb: 2,
+                        backgroundColor: active === "fitness" ? "#6d2ae2" : "transparent",
+                        color: active === "fitness" ? "#fff" : "#6d2ae2",
+                        borderColor: "#6d2ae2",
+                        '&:hover': {
+                            backgroundColor: active === "fitness" ? "#6d2ae2" : "#dfd8fd",
+                            color: active === "fitness" ? "#fff" : "#6d2ae2",
+                            borderColor: "#6d2ae2",
+                        }
+                    }}
+                    color="primary"
+                    onClick={() => setActive("fitness")}
+                    disabled
+                >
+                    Fitness Tracker <MdLock style={{ marginLeft: ".5rem" }} />
+                </Button>
+
+                <Button
+                    variant={active === "sleep" ? "contained" : "outlined"}
+                    sx={{
+                        mr: 2, mb: 2,
+                        backgroundColor: active === "sleep" ? "#6d2ae2" : "transparent",
+                        color: active === "sleep" ? "#fff" : "#6d2ae2",
+                        borderColor: "#6d2ae2",
+                        '&:hover': {
+                            backgroundColor: active === "sleep" ? "#6d2ae2" : "#dfd8fd",
+                            color: active === "sleep" ? "#fff" : "#6d2ae2",
+                            borderColor: "#6d2ae2",
+                        }
+                    }}
+                    color="primary"
+                    onClick={() => setActive("sleep")}
+                    disabled
+                >
+                    Sleep Tracker <MdLock style={{ marginLeft: ".5rem" }} />
+                </Button>
+            </Box>
+
+            {active === "budget" && <BudgetApp />}
+            {active === "loan" && <LoanCalculator />}
+        </React.Fragment>
     )
 }
 
