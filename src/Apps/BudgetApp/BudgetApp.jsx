9d74f1a5bbbba447454de6c1d6dd79e27c566520
Bhavya Khurana <bhavyakhurana002@gmail.com>
2023-08-21 02:02:36 +0530

    Improvement

--- src/Apps/BudgetApp/BudgetApp.jsx
+++ src/Apps/BudgetApp/BudgetApp.jsx
@@ -75,98 +75,104 @@ const BudgetApp = () => {
 
     return (
         <React.Fragment>
-            <Box mt={5} display="flex" justifyContent="space-between" alignItems="center" flexWrap="wrap" gap="1rem">
-                <Typography variant="h5">
-                    Budgets
-                </Typography>
+            <Box mt={5} p={2} sx={{
+                bgcolor: "#F3F0FF",
+                borderRadius: "4px"
+            }}>
 
-                <Box display="flex" alignItems="center" flexWrap="wrap" gap="0.5rem">
-                    <Button variant="contained" sx={{
-                        bgcolor: "#0e1313",
-                        color: "#dee2e6",
-                        '&:hover': {
+                <Box display="flex" justifyContent="space-between" alignItems="center" flexWrap="wrap" gap="1rem">
+                    <Typography variant="h5">
+                        Budgets
+                    </Typography>
+
+                    <Box display="flex" alignItems="center" flexWrap="wrap" gap="0.5rem">
+                        <Button variant="contained" sx={{
                             bgcolor: "#0e1313",
-                        }
-                    }}
-                        onClick={handleClickOpenBudget}
-                    >
-                        Add Budget
-                    </Button>
-                    <Button variant="outlined" sx={{
-                        borderColor: "#0e1313",
-                        color: "#0e1313",
-                        '&:hover': {
-                            borderColor: "#0e1313",
-                        }
-                    }}
-                        onClick={handleClickOpenExpense}
-                    >
-                        Add Expense
-                    </Button>
-                </Box>
-            </Box>
-
-            <Box mt={3}>
-                <Grid container spacing={2}>
-                    {
-                        budgets.map(budget => {
-
-                            const amount = getBudgetExpenses(budget.id).reduce((total, expense) => total += expense.amount, 0);
-
-                            return (
-                                <Grid item xs={12} sm={12} md={6} lg={4} key={budget.id}>
-                                    <BudgetCard
-                                        normal
-                                        name={budget.name}
-                                        amount={amount}
-                                        max={budget.max}
-                                        handleClickOpenViewExpenses={() => handleClickOpenViewExpenses(budget.id)}
-                                    />
-                                </Grid>
-                            )
-                        })
-                    }
-
-                    <Grid item xs={12} sm={12} md={6} lg={4}>
-                        <UncategorizedBudgetCard
-                            handleClickOpenViewExpenses={() => handleClickOpenViewExpenses(UNCATEGORIZED_BUDGET_ID)}
-                        />
-                    </Grid>
-                </Grid>
-            </Box>
-
-            {
-                totalAmount > 0 &&
-                <Box mt={3} sx={{
-                    width: { xs: "100%", sm: "100%", md: "50%", lg: "30%" }
-                }}>
-                    <Card className={classNames.join(" ")} sx={{
-                        border: "1px solid #f8f9fa",
-                        color: "#0e1313",
-                    }} elevation={0}>
-                        <CardHeader
-                            title="Total"
-                            action={
-                                <Typography variant="h6">
-                                    {currencyFormatter.format(totalAmount)} <span style={{ fontSize: "15px", opacity: "0.7" }}>/ {currencyFormatter.format(totalMax)}</span>
-                                </Typography>
+                            color: "#dee2e6",
+                            '&:hover': {
+                                bgcolor: "#0e1313",
                             }
-                        />
-                        <CardContent>
-                            <LinearProgress
-                                variant="determinate"
-                                color={getProgressBarVariant(totalAmount, totalMax)}
-                                value={totalAmount / totalMax * 100}
-                                sx={{
-                                    backgroundColor: totalAmount > totalMax && "#d32f2f",
-                                    height: "10px",
-                                    borderRadius: "5px",
-                                }}
-                            />
-                        </CardContent>
-                    </Card>
+                        }}
+                            onClick={handleClickOpenBudget}
+                        >
+                            Add Budget
+                        </Button>
+                        <Button variant="outlined" sx={{
+                            borderColor: "#0e1313",
+                            color: "#0e1313",
+                            '&:hover': {
+                                borderColor: "#0e1313",
+                            }
+                        }}
+                            onClick={handleClickOpenExpense}
+                        >
+                            Add Expense
+                        </Button>
+                    </Box>
                 </Box>
-            }
+
+                <Box mt={3}>
+                    <Grid container spacing={2}>
+                        {
+                            budgets.map(budget => {
+
+                                const amount = getBudgetExpenses(budget.id).reduce((total, expense) => total += expense.amount, 0);
+
+                                return (
+                                    <Grid item xs={12} sm={12} md={6} lg={4} key={budget.id}>
+                                        <BudgetCard
+                                            normal
+                                            name={budget.name}
+                                            amount={amount}
+                                            max={budget.max}
+                                            handleClickOpenViewExpenses={() => handleClickOpenViewExpenses(budget.id)}
+                                        />
+                                    </Grid>
+                                )
+                            })
+                        }
+
+                        <Grid item xs={12} sm={12} md={6} lg={4}>
+                            <UncategorizedBudgetCard
+                                handleClickOpenViewExpenses={() => handleClickOpenViewExpenses(UNCATEGORIZED_BUDGET_ID)}
+                            />
+                        </Grid>
+                    </Grid>
+                </Box>
+
+                {
+                    totalAmount > 0 &&
+                    <Box mt={3} sx={{
+                        width: { xs: "100%", sm: "100%", md: "50%", lg: "30%" }
+                    }}>
+                        <Card className={classNames.join(" ")} sx={{
+                            border: "1px solid #f8f9fa",
+                            color: "#0e1313",
+                        }} elevation={0}>
+                            <CardHeader
+                                title="Total"
+                                action={
+                                    <Typography variant="h6">
+                                        {currencyFormatter.format(totalAmount)} <span style={{ fontSize: "15px", opacity: "0.7" }}>/ {currencyFormatter.format(totalMax)}</span>
+                                    </Typography>
+                                }
+                            />
+                            <CardContent>
+                                <LinearProgress
+                                    variant="determinate"
+                                    color={getProgressBarVariant(totalAmount, totalMax)}
+                                    value={totalAmount / totalMax * 100}
+                                    sx={{
+                                        backgroundColor: totalAmount > totalMax && "#d32f2f",
+                                        height: "10px",
+                                        borderRadius: "5px",
+                                    }}
+                                />
+                            </CardContent>
+                        </Card>
+                    </Box>
+                }
+            </Box>
 
             <AddBudgetModal open={openBudget} handleClose={handleCloseBudget} />
             <AddExpenseModal open={openExpense} handleClose={handleCloseExpense} />
